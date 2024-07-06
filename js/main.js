d97175052c80dd7b5073c7fccdfc907915b72faf
NabiaHani <nabiahani02@gmail.com>
2024-07-06 17:02:45 +0500

    NH-PORTFOLIO

--- js/main.js
+++ js/main.js
@@ -1,68 +1,61 @@
-// Initiate the wowjs
-new WOW().init();
+// Toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
 
-// Back to top button
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 200) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
-});
-$('.back-to-top').click(function () {
-    $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
-    return false;
-});
+// Scroll section active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
 
+window.onscroll = () => {
+    let top = window.scrollY;
 
-// Sticky Navbar
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 0) {
-        $('.navbar').addClass('nav-sticky');
-    } else {
-        $('.navbar').removeClass('nav-sticky');
-    }
-});
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-
-
-// Typed Initiate
-if ($('.hero .hero-text h2').length == 1) {
-    var typed_strings = $('.hero .hero-text .typed-text').text();
-    var typed = new Typed('.hero .hero-text h2', {
-        strings: typed_strings.split(', '),
-        typeSpeed: 100,
-        backSpeed: 20,
-        smartBackspace: false,
-        loop: true
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                let activeLink = document.querySelector('header .navbar a[href*=' + id + ']');
+                if (activeLink) {
+                    activeLink.classList.add('active');
+                }
+            });
+        };
     });
-}
 
-(jQuery);
+    // Sticky navbar
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 
+    // Remove toggle icon and navbar when scrolling
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
 
-// ============== SETRVICES SECTION READMORE BUTTON ====================
-const servicesBtn1 = document.getElementById('serviceBtn1');
-const servicesText1 = document.querySelector('.services-box p .moreText1');
-
-servicesBtn1.addEventListener('click', () => {
-  servicesText1.classList.toggle('show');
-  servicesBtn1.textContent = servicesText1.classList.contains('show') ? 'Read Less' : 'Read More';
+// scroll reveal
+ScrollReveal({
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
 });
 
-const servicesBtn2 = document.getElementById('serviceBtn2');
-const servicesText2 = document.querySelector('.services-box p .moreText2');
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-contact h1, .about-img, .skills-column', { origin: 'left'});
+ScrollReveal().reveal('.home-contact p, .about-content, .title', { origin: 'right'});
 
-servicesBtn2.addEventListener('click', () => {
-  servicesText2.classList.toggle('show');
-  servicesBtn2.textContent = servicesText2.classList.contains('show') ? 'Read Less' : 'Read More';
-});
-
-const servicesBtn3 = document.getElementById('serviceBtn3');
-const servicesText3 = document.querySelector('.services-box p .moreText3');
-
-servicesBtn3.addEventListener('click', () => {
-  servicesText3.classList.toggle('show');
-  servicesBtn3.textContent = servicesText3.classList.contains('show') ? 'Read Less' : 'Read More';
+// typed js
+const typed = new Typed('.multiple-text',{
+    strings: ['Frontend Developer', 'Web Designer', 'UI/UX Designer', 'Wordpress Developer', 'Freelancer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
 });
